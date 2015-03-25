24e78ed70f30d0ec4b2244dcce79a6df3d5a5c04
tbrocail <t.brocail@dreamcentury.com>
2015-03-25 17:20:03 +0100

    VÃ©rification globale

--- App/Frontend/Modules/News/NewsController.php
+++ App/Frontend/Modules/News/NewsController.php
@@ -3,6 +3,7 @@ namespace App\Frontend\Modules\News;
 
 use \OCFram\BackController;
 use \OCFram\HTTPRequest;
+use \Entity\Comment;
 
 class NewsController extends BackController
 {
@@ -35,4 +36,49 @@ class NewsController extends BackController
     // On ajoute la variable $listeNews à la vue.
     $this->page->addVar('listeNews', $listeNews);
   }
+  public function executeInsertComment(HTTPRequest $request)
+  {
+    $this->page->addVar('title', 'Ajout d\'un commentaire');
+    
+    if ($request->postExists('pseudo'))
+    {
+      $comment = new Comment([
+        'news' => $request->getData('news'),
+        'auteur' => $request->postData('pseudo'),
+        'contenu' => $request->postData('contenu')
+      ]);
+      
+      if ($comment->isValid())
+      {
+        $this->managers->getManagerOf('Comments')->save($comment);
+        
+        $this->app->user()->setFlash('Le commentaire a bien été ajouté, merci !');
+        
+        $this->app->httpResponse()->redirect('news-'.$request->getData('news').'.html');
+      }
+      else
+      {
+        $this->page->addVar('erreurs', $comment->erreurs());
+      }
+      
+      $this->page->addVar('comment', $comment);
+    }
+  }
+
+  public function executeShow(HTTPRequest $request)
+  {
+    $news = $this->managers->getManagerOf('News')->getUnique($request->getData('id'));
+    
+    if (empty($news))
+    {
+      $this->app->httpResponse()->redirect404();
+    }
+    
+    $this->page->addVar('title', $news->titre());
+    $this->page->addVar('news', $news);
+    $this->page->addVar('comments', $this->managers->getManagerOf('Comments')->getListOf($news->id()));
+  }
+
+  
 }
+?>
