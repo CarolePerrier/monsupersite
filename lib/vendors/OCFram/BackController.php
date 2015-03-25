95c7204683fe6e1d7175d36861beaef497bd7b6f
tbrocail <t.brocail@dreamcentury.com>
2015-03-25 15:02:25 +0100

    Ajout de la mÃ©thode setView() dans la classe BackController

--- lib/vendors/OCFram/BackController.php
+++ lib/vendors/OCFram/BackController.php
@@ -67,5 +67,18 @@ abstract class BackController extends ApplicationComponent
 
     $this->view = $view;
   }
+
+  public function setView($view)
+  {
+    if (!is_string($view) || empty($view))
+    {
+      throw new \InvalidArgumentException('La vue doit être une chaine de caractères valide');
+    }
+    
+    $this->view = $view;
+    
+    $this->page->setContentFile(__DIR__.'/../../App/'.$this->app->name().'/Modules/'.$this->module.'/Views/'.$this->view.'.php');
+  }
+}
 }
 ?>
