0ecf1b3d5229cde0a8d1d968d5809bb2ffbf38da
tbrocail <t.brocail@dreamcentury.com>
2015-03-25 15:42:55 +0100

    Ajout de la classe FrontendApplication

---
+++ App/Frontend/FrontendApplication.php
@@ -0,0 +1,25 @@
+<?
+	namespace App\Frontend;
+
+	//on va chercher l'application
+	use \OCFram\Application;
+
+	class FrontendApplication extends Application
+	{
+		public function __construct()
+		{
+			//On utilise le contructeur de la classe mère
+			parent::__construct();
+			//On rajoute l'instaciation du nom
+			$this->name = 'Frontend';
+		}
+	
+		public function run()
+		{
+			$controller = $this->getController();
+			$controller->execute();
+
+			$this->httpResponse->setPage($controller->page());
+			$this->httpResponse->send();
+		}
+	}
