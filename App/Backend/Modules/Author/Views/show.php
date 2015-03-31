9f7cf688a425905239987e708531e07ac3b7062c
tbrocail <t.brocail@dreamcentury.com>
2015-03-31 16:14:27 +0200

    The link to the news by authors is now full operational (with the correct url)

--- App/Backend/Modules/Author/Views/show.php
+++ App/Backend/Modules/Author/Views/show.php
@@ -13,7 +13,7 @@ if (empty($authors))
 <?php foreach ($authors as $author) { ?>
 	<p>
 		<?php
-     		echo '<tr><td><a href="/admin/author-', $author['id'],'.html"><img src="/images/list.png" alt="Liste" width="20" height="20"/></a></td>&nbsp;<td>', $author['pseudo'], '</td>&nbsp;<td> ', $author['firstname'], '</td>&nbsp;<td> ', $author['lastname'], '</td>&nbsp;<td>', '</td><td><a href="author-update-', $author['pseudo'], '.html"><img src="/images/update.png" alt="Modifier" /></a>&nbsp;<a href="author-delete-', $author['id'], '.html"><img src="/images/delete.png" alt="Supprimer" /></a></td></tr>', "\n";
+     		echo '<tr><td><a href="/admin/author-', $author['id'],'/', $author['firstname'],'-', $author['lastname'],'.html"><img src="/images/list.png" alt="Liste" width="20" height="20"/></a></td>&nbsp;<td>', $author['pseudo'], '</td>&nbsp;<td> ', $author['firstname'], '</td>&nbsp;<td> ', $author['lastname'], '</td>&nbsp;<td>', '</td><td><a href="author-update-', $author['pseudo'], '.html"><img src="/images/update.png" alt="Modifier" /></a>&nbsp;<a href="author-delete-', $author['id'], '.html"><img src="/images/delete.png" alt="Supprimer" /></a></td></tr>', "\n";
       	?>
 	</p>
 <?php } ?>
